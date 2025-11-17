55eee86c58f4294d218f5332cf4f2ea1739f7958
MUTHUSURESH N <muthusuresh.240198@aids.ritchennai.edu.in>
2025-11-18 01:45:28 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const ContactRoutes = require('./routes/contact');
 const AdminRoutes = require('./routes/users');
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://decode-studio.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
