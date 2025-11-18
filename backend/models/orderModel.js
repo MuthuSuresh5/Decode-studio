6c5bd9b2ac0ce0b12e0b652be91831dd94a900c3
MUTHUSURESH N <muthusuresh.240198@aids.ritchennai.edu.in>
2025-11-18 23:08:45 +0530

    Initial commit

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,7 +4,6 @@ const User = require('./UserModel')
 const orderSchema = new mongoose.Schema({
     phoneNumber:{
         type: Number,
-        unique: true,
         required: true
     },
     service:{
